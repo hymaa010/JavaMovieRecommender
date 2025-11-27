bcac1593526413a2c037476e936924e0e994794a
ibrahim atef <hymaa013@gmail.com>
2025-11-27 16:57:08 +0200

    CD

---
+++ src/test/UserInputTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+public class UserInputTest {
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+    }
+
+    @Test
+    void testDefaultConstructor() {
+        assertEquals("", user.getUserName());
+        assertEquals("", user.getUserId());
+        assertNotNull(user.getMoviesIds());
+        assertEquals(20, user.getMoviesIds().length);
+    }
+
+    @Test
+    void testSetAndGetUserName() {
+        user.setUserName("Mazen Mohamed");
+        assertEquals("Mazen Mohamed", user.getUserName());
+    }
+
+    @Test
+    void testSetAndGetUserId() {
+        user.setUserId("123456789");
+        assertEquals("123456789", user.getUserId());
+    }
+
+    @Test
+    void testSetAndGetMoviesIds() {
+        String[] movies = {"DSD123", "KWR456", "MJQ789"};
+        user.setMoviesIds(movies);
+        assertArrayEquals(movies, user.getMoviesIds());
+    }
+
+    @Test
+    void testMoviesIdsDefaultValuesAreNull() {
+        String[] arr = user.getMoviesIds();
+        for (String s : arr) {
+            assertNull(s);
+        }
+    }
+}
