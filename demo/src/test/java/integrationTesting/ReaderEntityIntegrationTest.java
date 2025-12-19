b25c32e20bea65e09529a388e8c0aeabe5f22f84
ibrahim atef <hymaa013@gmail.com>
2025-12-19 18:48:47 +0200

    split test files into folders

--- demo/src/test/java/integrationTesting/ReaderEntityIntegrationTest.java
+++ demo/src/test/java/integrationTesting/ReaderEntityIntegrationTest.java
@@ -1,3 +1,4 @@
+package integrationTesting;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
--- demo/src/test/java/integrationTesting/RecommendationEndToEndTest.java
+++ demo/src/test/java/integrationTesting/RecommendationEndToEndTest.java
@@ -1,3 +1,4 @@
+package integrationTesting;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
